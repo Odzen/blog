b02b30d5a8ce1f6adcdb4de06f9b21d05c0e1ea9
Odzen <jsebastian.va@gmail.com>
2023-09-05 23:59:50 -0600

    mock up - - done

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<h1>About</h1>
+<p>
+	Colombian currently studying at <a href="https://tec.mx/es" target="_blank">Tec de Monterrey</a>
+	for an exchange semester, pursuing the Computer Science undergraduate degree and working at
+	<a href="https://soymacondo.com" target="_blank">Macondo</a> as a Junior Software Engineer. 💻 Like:
+	Sports, dancing, reading, learn new things. ⚽ 📕 Speak: Spanish, English, Russian.
+</p>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>About</svelte:head>
+
+<h2>Contact</h2>
+
+<ul>
+	<li><a href="https://www.linkedin.com/in/juanvelasquezacevedo/" target="_blank">LinkedIn</a></li>
+	<li><a href="https://github.com/Odzen" target="_blank">GitHub</a></li>
+</ul>
